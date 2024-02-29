f4182249c1f1a518a71ce2f79ecfacb466428f26
DanielAlonsoD <daniel.alodom@sanviatorvalladolid.com>
2024-02-29 10:52:08 +0100

    Primera pantalla comenzada.

--- app/src/main/java/com/example/focuson/MainActivity.java
+++ app/src/main/java/com/example/focuson/MainActivity.java
@@ -1,16 +1,62 @@
 package com.example.focuson;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private FirebaseAuth autenticacion;
+    private EditText textoCorreo, textoContrasena;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        FirebaseApp.initializeApp(this);
+        autenticacion = FirebaseAuth.getInstance();
+
+        textoCorreo = findViewById(R.id.textoInsertarCorreo);
+        textoContrasena = findViewById(R.id.textoInsertarContrasena);
+        Button botonInicio = findViewById(R.id.botonIniciarSesion);
+        Button botonRegistrarse = findViewById(R.id.botonRegistrarse);
+        Button botonGoogle = findViewById(R.id.botonGoogle);
+
+        botonInicio.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String correo = textoCorreo.getText().toString();
+        String contrasena = textoContrasena.getText().toString();
+
+        if (correo.isEmpty() || contrasena.isEmpty()) {
+
+        } else {
+            Log.i("Escrito todo", "Correo Y Contraseña escritos");
+            autenticacion.signInWithEmailAndPassword(correo, contrasena)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            Log.i("Ejecutando", "Ejecutando el auth");
+                            if (task.isSuccessful()) {
+                                Intent actividadMenu = new Intent(MainActivity.this, MenuActivity.class);
+                            }
+                        }
+                    });
+        }
     }
 }
