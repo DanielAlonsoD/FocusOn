035d8e1213cc97595def28a026834988f301e0a8
DanielAlonsoD <daniel.alodom@sanviatorvalladolid.com>
2024-06-07 12:36:53 +0200

    Temporizadores terminados (a excepción de al pulsar hacia atrás que funcione correctamente (que no funciona bien).

--- app/src/main/java/com/example/focuson/IniciarTemporizadorActivity.java
+++ app/src/main/java/com/example/focuson/IniciarTemporizadorActivity.java
@@ -1,40 +1,158 @@
 package com.example.focuson;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.content.res.ColorStateList;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import tablas.Temporizador;
 
 public class IniciarTemporizadorActivity extends AppCompatActivity implements View.OnClickListener {
+    private Temporizador temporizador;
+    private TextView textoTipoTiempo, textoTiempo;
+    private MaterialButton botonIniciar;
+    private Timer timer;
+    private TimerTask timerTask;
+    private double tiempo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iniciar_temporizador);
 
-        Temporizador temporizador = getIntent().getParcelableExtra("Temporizador");
+        temporizador = getIntent().getParcelableExtra("Temporizador");
 
         MaterialToolbar encabezado = findViewById(R.id.encabezadoIniciarTemporizador);
-        TextView textoTipoTiempo = findViewById(R.id.textoTipoTiempo);
-        TextView textoTiempo = findViewById(R.id.textoTiempo);
-        MaterialButton botonIniciar = findViewById(R.id.botonIniciar);
+        textoTipoTiempo = findViewById(R.id.textoTipoTiempo);
+        textoTiempo = findViewById(R.id.textoTiempo);
+        botonIniciar = findViewById(R.id.botonIniciar);
         MaterialButton botonPausar = findViewById(R.id.botonPausar);
         MaterialButton botonReiniciar = findViewById(R.id.botonReiniciar);
 
         encabezado.setTitle(temporizador.getTitulo());
         textoTiempo.setText(temporizador.getTrabajo());
 
+        timer = new Timer();
+        tiempo = getTiempo(temporizador.getTrabajo());
+
         encabezado.setNavigationOnClickListener(this);
+        botonIniciar.setOnClickListener(this);
+        botonPausar.setOnClickListener(this);
+        botonReiniciar.setOnClickListener(this);
+    }
+
+    public double getTiempo(String tiempo) {
+        String[] tiempos = tiempo.split(":");
+        double horas = Double.parseDouble(tiempos[0])*3600;
+        double minutos = Double.parseDouble(tiempos[1])*60;
+        double segundos = Double.parseDouble(tiempos[2]);
+        return horas+minutos+segundos;
     }
 
     @Override
     public void onClick(View v) {
-        getOnBackPressedDispatcher().onBackPressed();
+        if (v.getId() == R.id.botonIniciar) {
+            cambioEstiloBoton(botonIniciar, false);
+
+        } else if (v.getId() == R.id.botonPausar) {
+            cambioEstiloBoton(botonIniciar, true);
+        } else if (v.getId() == R.id.botonReiniciar) {
+            MaterialAlertDialogBuilder alerta = new MaterialAlertDialogBuilder(v.getContext(), R.style.Alertas);
+            alerta.setTitle(R.string.textoReiniciarTemporizador).setMessage(R.string.descripcionReiniciarTemporizador)
+                    .setPositiveButton(R.string.textoSi, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            textoTipoTiempo.setText(R.string.textoTrabajo);
+                            textoTiempo.setText(temporizador.getTrabajo());
+                            cambioEstiloBoton(botonIniciar, true);
+                            try {
+                                timerTask.cancel();
+                            } catch (NullPointerException ex) {}
+                            tiempo = getTiempo(temporizador.getTrabajo());
+                        }
+                    }).setNeutralButton(R.string.textoCancelar, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {}});
+            alerta.show();
+        } else {
+            try {
+                timerTask.cancel();
+            } catch (NullPointerException ex) {}
+            getOnBackPressedDispatcher().onBackPressed();
+        }
+    }
+
+    public void cambioEstiloBoton (MaterialButton boton, boolean habilitado) {
+        if (habilitado) {
+            boton.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.scampi));
+            boton.setEnabled(true);
+            try {
+                timerTask.cancel();
+            } catch (NullPointerException ex) {}
+        } else {
+            boton.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.blue_bell));
+            boton.setEnabled(false);
+            comenzarTiempo();
+        }
+    }
+
+    private void comenzarTiempo()
+    {
+        timerTask = new TimerTask()
+        {
+            @Override
+            public void run()
+            {
+                runOnUiThread(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        if (tiempo!=0) {
+                            tiempo--;
+                            textoTiempo.setText(getTiempoRestante());
+                        } else if (textoTipoTiempo.getText().equals("Trabajo")) {
+                            tiempo = getTiempo(temporizador.getDescanso());
+                            textoTipoTiempo.setText(R.string.textoDescanso);
+                            textoTiempo.setText(temporizador.getDescanso());
+                            MediaPlayer.create(IniciarTemporizadorActivity.this, R.raw.start_sound_beep).start();
+                        } else {
+                            tiempo = getTiempo(temporizador.getTrabajo());
+                            textoTipoTiempo.setText(R.string.textoTrabajo);
+                            textoTiempo.setText(temporizador.getTrabajo());
+                            MediaPlayer.create(IniciarTemporizadorActivity.this, R.raw.start_sound_beep).start();
+                        }
+                    }
+                });
+            }
+
+        };
+        timer.scheduleAtFixedRate(timerTask, 0 ,1000);
+    }
+
+
+    private String getTiempoRestante()
+    {
+        int redondear = (int) Math.round(tiempo);
+
+        int segundos = ((redondear % 86400) % 3600) % 60;
+        int minutos = ((redondear % 86400) % 3600) / 60;
+        int horas = ((redondear % 86400) / 3600);
+
+        return String.format("%02d",horas) + ":" + String.format("%02d",minutos) + ":" + String.format("%02d",segundos);
     }
 }
